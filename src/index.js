352e92d5c490dbceede95c1542b9ffa21d8c6050
Quendara <andregarstka@web.de>
2020-02-12 21:44:32 +0100

    Fetch groups from API

--- src/index.js
+++ src/index.js
@@ -21,35 +21,6 @@ import "antd/dist/antd.css";
 
 const { Header, Footer, Sider, Content } = Layout;
 
-// https://jerairrest.github.io/react-chartjs-2/
-
-const fakeResponse = {};
-fakeResponse["andre"] = [
-  {
-    group_name: "Andres Auto",
-    group_id: 2,
-    group_unit: "km"
-  },
-  {
-    group_name: "Strom",
-    group_id: 1,
-    group_unit: "kWh"
-  },
-  {
-    group_name: "Dummy",
-    group_id: 99,
-    group_unit: "km"
-  }
-];
-
-fakeResponse["irena"] = [
-  {
-    group_name: "Irenas Auto",
-    group_id: 3,
-    group_unit: "km"
-  }
-];
-
 // A custom hook that builds on useLocation to parse
 // the query string for you.
 function useQuery() {
@@ -70,28 +41,30 @@ class TimeSeries extends React.Component {
       location: new URLSearchParams(props.location.search)
     };
 
-    this.resource = "group/1/data";
+    
   }
 
   componentDidMount() {
     const user = this.state.location.get("user");
 
+    this.resource = "groups/" + user;
+
     fetch(Settings.baseAwsUrl + this.resource)
       .then(res => res.json())
       .then(
         result => {
-          if (fakeResponse[user] === undefined) {
+
+//          {
+//              group_name: "Dummy",
+//              group_id: 99,
+//              group_unit: "km"
+//          }
+          
             this.setState({
               isLoaded: true,
-              timeseries: []
+              timeseries: result
             });
-          } else {
-            // TODO get REAL RESPONSE FORM DB
-            this.setState({
-              isLoaded: true,
-              timeseries: fakeResponse[user]
-            });
-          }
+          
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
