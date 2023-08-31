92e426fb8cb1b1126f12247e18665c41e5e89de7
Andre Garstka <andregarstka@web.de>
2023-08-31 21:10:53 +0200

    $$ supports voutube

--- src/components/MarkdownComponents.tsx
+++ src/components/MarkdownComponents.tsx
@@ -26,19 +26,52 @@ function isAlertColor(color: string): boolean {
 
 export const getAlertJSX = (line: string) => {
 
-        let extractedString = line.split(":").at(1)
-        let severity: AlertColor = "success"
+    let extractedString = line.split(":").at(1)
+    let severity: AlertColor = "success"
 
-        if (extractedString?.split("/").length === 2) {
-            const arr = extractedString?.split("/")
-            severity = isAlertColor(arr[0]) ? arr[0] as AlertColor : "success"
-            extractedString = arr[1]
+    if (extractedString?.split("/").length === 2) {
+        const arr = extractedString?.split("/")
+        severity = isAlertColor(arr[0]) ? arr[0] as AlertColor : "success"
+        extractedString = arr[1]
+    }
+
+    return (
+        <Box mt={1} mb={1} >
+            <Alert severity={severity}>{extractedString}</Alert>
+        </Box>
+    )
+}
+
+export const getVideoJSX = (line: string) => {
+
+    let indexOf = line.indexOf(":")
+    if (indexOf) {
+
+        const videoUrl = line.slice(indexOf + 1, line.length).trim()
+        if (videoUrl) {
+
+            const prefix = "https://www.youtube.com/embed/"
+
+            return (
+                <Box mt={1} mb={1} >
+                    {/* {videoUrl} <br /> */}
+                    <iframe style={{ border:0, aspectRatio:"4/3" }} width="100%" src={prefix + videoUrl}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    
+                    allowFullScreen
+                    >
+                    </iframe>
+                </Box>
+            )
         }
+    }
 
-        return (
-            <Box mt={1} mb={1} >
-                <Alert severity={severity}>{extractedString}</Alert>
-            </Box>
-        )
-   
+
+
+
+    return (
+        <Box mt={1} mb={1} >
+            <Alert severity={"error"}>{"Unexpected video"}</Alert>
+        </Box>
+    )
 }
