6fc034294c1525e8c05a1d0915c9e56208e9a4db
andregarstka@web.de <andregarstka@web.de>
2024-11-21 21:48:23 +0100

    Refactored abc player

--- src/pages/AbcPlayer.tsx
+++ src/pages/AbcPlayer.tsx
@@ -1,3 +1,131 @@
-export const AbcPlayer = (props: SongProps) => {
-    
+import React from "react";
+import { useRef, useEffect } from "react";
+import { getUniqueId } from "../components/helpers";
+
+import abcjs from "abcjs";
+import 'abcjs/abcjs-audio.css';
+
+
+interface Props {
+    play: string
+}
+
+export const AbcPlayer = (props: Props) => {
+
+    const paperId = getUniqueId()
+
+
+    const paperRef = useRef<HTMLDivElement | null>(null);
+    const cursorRef = useRef<SVGLineElement | null>(null);
+
+
+    const CursorControl = () => {
+        // const cursorRef = useRef<SVGLineElement | null> (null);
+        // let cursor : SVGLineElement;
+
+
+
+        const onStart = () => {
+
+            console.log("Cursor created")
+
+            const paper: any = document.getElementById("songPaper" + paperId)
+            // if( cursorRef.current === null ) return;
+
+            const svg = paperRef.current?.querySelector("svg");
+
+            // console.log("Cursor created paper", paper)
+            // console.log("Cursor created svg", svg)
+
+            if (cursorRef.current === null) {
+                cursorRef.current = document.createElementNS("http://www.w3.org/2000/svg", "line");
+            }
+
+            cursorRef.current.setAttribute("class", "abcjs-cursor");
+            cursorRef.current.setAttribute("x1", "0");
+            cursorRef.current.setAttribute("y1", "0");
+            cursorRef.current.setAttribute("x2", "0");
+            cursorRef.current.setAttribute("y2", "0");
+
+            svg?.appendChild(cursorRef.current);
+        };
+
+        const onEvent = (event: any) => {
+            console.log("onEvent playback", event)
+
+            // Das Element, das hervorgehoben wird
+            // if (event.measureStart && event.left) {
+            //     const element = document.querySelector(`[data-abcjs="${event.left}"]`) as HTMLElement;;
+            //     if (element) {
+
+            //         element.scrollIntoView({ behavior: "smooth", block: "center" });
+            //         element.style.backgroundColor = "yellow"; // Beispiel für visuelle Hervorhebung
+            //     }
+            // }
+
+            if (cursorRef.current) {
+                console.log("cursor found", event.left)
+                cursorRef.current.setAttribute("x1", "" + (event.left - 2));
+                cursorRef.current.setAttribute("x2", "" + (event.left - 2));
+                cursorRef.current.setAttribute("y1", event.top);
+                cursorRef.current.setAttribute("y2", event.top + event.height);
+
+            }
+        }
+        const onFinished = () => console.log("Finished playback")
+
+        return { onStart, onEvent, onFinished };
+    };
+
+
+    useEffect(() => {
+
+
+        const tunes = abcjs.renderAbc("songPaper" + paperId, props.play, { responsive: "resize" });
+        if (abcjs.synth.supportsAudio()) {
+            const synth = new abcjs.synth.CreateSynth();
+            const controlOptions = {
+                displayRestart: true,
+                displayPlay: true,
+                displayProgress: true,
+                displayClock: true,
+            };
+
+            const cursorControl = CursorControl();
+
+            const synthControl = new abcjs.synth.SynthController();
+            synthControl.load("#audio" + paperId, cursorControl, controlOptions);
+
+            synth
+                .init({
+                    visualObj: tunes[0],
+                    // audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+                    audioContext: new (window.AudioContext)()
+                })
+                .then(() => {
+                    synthControl.setTune(tunes[0], true).then(() => {
+                        console.log("Synthesizer ready, playing tune...");
+                        synthControl.restart();
+                    });
+                })
+                .catch((error) => {
+                    console.error("Error initializing the synthesizer:", error);
+                });
+
+        } else {
+            console.log("Audio is not supported on this browser");
+        }
+
+    }, [props.play]);
+
+    return (
+        <>
+
+            <div id={"songPaper" + paperId} ref={paperRef} />
+            {/* <Button onClick={ activate }>Play</Button> */}
+            <div id={"audio" + paperId}></div>
+        </>
+
+    )
+
 }
--- src/pages/Piano.tsx
+++ src/pages/Piano.tsx
@@ -3,10 +3,8 @@ import { grid } from "@mui/system";
 import React, { useEffect, useRef, useState } from "react"
 import { useParams } from "react-router-dom";
 
-import abcjs from "abcjs";
-import 'abcjs/abcjs-audio.css';
-import { getUniqueId } from "../components/helpers";
-import { VisualCursorPlayback } from "./AbcPlayer"
+
+import { AbcPlayer } from "./AbcPlayer"
 
 interface SongProps {
     play: string
@@ -22,7 +20,6 @@ interface PianoProps {
 }
 
 export const createPianoClasses = (showNodes: boolean) => {
-
 
     const keyDistance = 25
     const height = 3 * keyDistance
@@ -96,148 +93,42 @@ export const PianoSong = (props: SongProps) => {
     const [play, setPlay] = useState(props.play);
     const parts = play ? play.split("\n") : []
 
-    const paperId = getUniqueId()
-
-    const paperRef = useRef<HTMLDivElement | null>(null);
-    const cursorRef = useRef<SVGLineElement | null> (null);
 
 
-    const CursorControl = () => {
-        // const cursorRef = useRef<SVGLineElement | null> (null);
-        // let cursor : SVGLineElement;
 
-
-        
-        const onStart = () => {
-
-            console.log("Cursor created")
-            
-            const paper : any = document.getElementById( "songPaper" + paperId )
-            // if( cursorRef.current === null ) return;
-
-            const svg = paperRef.current?.querySelector("svg");
-
-            // console.log("Cursor created paper", paper)
-            // console.log("Cursor created svg", svg)
-
-            if( cursorRef.current === null ){
-                cursorRef.current = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    return (
+        <>
+            {props.showTextinput &&
+                <div className="no-print" >
+                    <TextField
+                        value={play}
+                        // error={trySend}
+                        // fullWidth
+                        defaultValue={play}
+                        multiline
+                        sx={{ m: 8, width: "50vw" }}
+                        variant="outlined"
+                        // className={getInputClass(username)}
+                        label="Name"
+                        onChange={e => setPlay(e.target.value)}
+                    />
+                </div>
             }
 
-            cursorRef.current.setAttribute("class", "abcjs-cursor");
-            cursorRef.current.setAttribute("x1", "0");
-            cursorRef.current.setAttribute("y1", "0");
-            cursorRef.current.setAttribute("x2", "0");
-            cursorRef.current.setAttribute("y2", "0");
+            {props.showAbcOnly ? <>
+                <AbcPlayer play={ play } />
+            </> :
 
-            svg?.appendChild(cursorRef.current);
-        };
-
-        const onEvent = (event: any) => {
-            console.log("onEvent playback", event)
-
-            // Das Element, das hervorgehoben wird
-            // if (event.measureStart && event.left) {
-            //     const element = document.querySelector(`[data-abcjs="${event.left}"]`) as HTMLElement;;
-            //     if (element) {
-
-            //         element.scrollIntoView({ behavior: "smooth", block: "center" });
-            //         element.style.backgroundColor = "yellow"; // Beispiel für visuelle Hervorhebung
-            //     }
-            // }
-
-            if (cursorRef.current) {
-                console.log( "cursor found", event.left )
-                cursorRef.current.setAttribute("x1", ""+(event.left - 2));
-                cursorRef.current.setAttribute("x2", ""+(event.left - 2));
-                cursorRef.current.setAttribute("y1", event.top);
-                cursorRef.current.setAttribute("y2", event.top + event.height);
-
-            }            
-        }
-        const onFinished = () => console.log("Finished playback")
-
-        return { onStart, onEvent, onFinished };
-    };
-
-
-useEffect(() => {
-
-
-    const tunes = abcjs.renderAbc("songPaper" + paperId, play, { responsive: "resize" });
-    if (abcjs.synth.supportsAudio()) {
-        const synth = new abcjs.synth.CreateSynth();
-        const controlOptions = {
-            displayRestart: true,
-            displayPlay: true,
-            displayProgress: true,
-            displayClock: true,
-        };
-
-        if (props.showAbcOnly) {
-
-            const cursorControl = CursorControl();
-
-            const synthControl = new abcjs.synth.SynthController();
-            synthControl.load("#audio" + paperId, cursorControl, controlOptions);
-
-            synth
-                .init({
-                    visualObj: tunes[0],
-                    // audioContext: new (window.AudioContext || window.webkitAudioContext)(),
-                    audioContext: new (window.AudioContext)()
-                })
-                .then(() => {
-                    synthControl.setTune(tunes[0], true).then(() => {
-                        console.log("Synthesizer ready, playing tune...");
-                        synthControl.restart();
-                    });
-                })
-                .catch((error) => {
-                    console.error("Error initializing the synthesizer:", error);
-                });
-        }
-    } else {
-        console.log("Audio is not supported on this browser");
-    }
-
-}, [play]);
-
-return (
-    <>
-        {props.showTextinput &&
-            <div className="no-print" >
-                <TextField
-                    value={play}
-                    // error={trySend}
-                    // fullWidth
-                    defaultValue={play}
-                    multiline
-                    sx={{ m: 8, width: "50vw" }}
-                    variant="outlined"
-                    // className={getInputClass(username)}
-                    label="Name"
-                    onChange={e => setPlay(e.target.value)}
-                />
-            </div>
-        }
-
-        {props.showAbcOnly ? <>
-            <div id={"songPaper" + paperId} ref={paperRef} />
-            {/* <Button onClick={ activate }>Play</Button> */}
-            <div id={"audio" + paperId}></div>
-        </> :
-
-            <Grid container spacing={4} >
-                {parts.map(part => (
-                    <Grid item xs={12} md={6} >
-                        <PianoPart play={part} showNodes={props.showNodes} />
-                    </Grid>
-                ))
-                }
-            </Grid>}
-    </>
-)
+                <Grid container spacing={4} >
+                    {parts.map(part => (
+                        <Grid item xs={12} md={6} >
+                            <PianoPart play={part} showNodes={props.showNodes} />
+                        </Grid>
+                    ))
+                    }
+                </Grid>}
+        </>
+    )
 }
 
 export const PianoPart = (props: SongProps) => {
@@ -342,24 +233,17 @@ export const Piano = (props: PianoProps) => {
             return {}
         }
     }
-
     const playabc = "K:C\nL:1/4\n [ " + props.play.join("") + "]\n"
-
-
 
     useEffect(() => {
 
-
-        abcjs.renderAbc("notepaper" + playabc, playabc)// "X:1\nK:D\nDD AA|BBA2|\n");
+        // abcjs.renderAbc("notepaper" + playabc, playabc)// "X:1\nK:D\nDD AA|BBA2|\n");
+        <AbcPlayer play={playabc} />
         console.log(playabc)
 
     }, [props.play]);
 
     const pianoClasses = createPianoClasses(props.showNodes)
-
-
-
-
 
     return (<>
         <Box sx={pianoClasses.layout}>
